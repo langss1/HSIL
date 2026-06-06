a68692759a31d435524ea65ad32b6277c6590d39
arinanabila <areenrhmania@gmail.com>
2026-06-06 14:30:20 +0700

    [Arina Rahmania] - Mengembalikan radius validasi GPS normal ke jarak 500m

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -16,7 +16,7 @@ class AppConstants {
   static const String officeName          = 'Telkom University Bandung';
   static const double officeLatitude      = -6.97328;
   static const double officeLongitude     = 107.63034;
-  static const double officeRadiusMeters  = 1500;
+  static const double officeRadiusMeters  = 500;
 
   // Timing
   static const Duration splashDuration   = Duration(milliseconds: 1500);
